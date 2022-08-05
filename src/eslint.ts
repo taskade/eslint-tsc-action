abd9ccbf60e4ad911caa5f1feb7ce2dcc567c566
Daniel Lee <minqiang8194@gmail.com>
2022-08-05 11:17:07 +0800

    fix: attempt to reduce heap usage

--- src/eslint.ts
+++ src/eslint.ts
@@ -17,38 +17,38 @@ export default async function eslint(
   const { ESLint } = (await import(modulePath)) as typeof import('eslint');
 
   const eslint = new ESLint();
-  const results = await eslint.lintFiles([...filesToLint]);
-
   const annotations: Annotation[] = [];
+  for (const file of filesToLint) {
+    const results = await eslint.lintFiles(file);
+    for (const result of results) {
+      const { filePath, messages } = result;
 
-  for (const result of results) {
-    const { filePath, messages } = result;
+      for (const msg of messages) {
+        const { line, endLine, severity, ruleId, message, column, endColumn } =
+          msg;
 
-    for (const msg of messages) {
-      const { line, endLine, severity, ruleId, message, column, endColumn } =
-        msg;
+        const filePathRelative = path.relative(process.cwd(), filePath);
+        const start_line = line || 0;
+        const end_line = endLine || line || 0;
+        const annotation_level = ESLINT_GITHUB_LEVELS[severity];
+        const title = ruleId ? `ESLint: ${ruleId}` : 'ESLint';
 
-      const filePathRelative = path.relative(process.cwd(), filePath);
-      const start_line = line || 0;
-      const end_line = endLine || line || 0;
-      const annotation_level = ESLINT_GITHUB_LEVELS[severity];
-      const title = ruleId ? `ESLint: ${ruleId}` : 'ESLint';
+        const annotation: Annotation = {
+          path: filePathRelative,
+          start_line,
+          end_line,
+          annotation_level,
+          title,
+          message,
+        };
 
-      const annotation: Annotation = {
-        path: filePathRelative,
-        start_line,
-        end_line,
-        annotation_level,
-        title,
-        message,
-      };
+        if (annotation.start_line === annotation.end_line) {
+          annotation.start_column = column || 0;
+          annotation.end_column = endColumn || column || 0;
+        }
 
-      if (annotation.start_line === annotation.end_line) {
-        annotation.start_column = column || 0;
-        annotation.end_column = endColumn || column || 0;
+        annotations.push(annotation);
       }
-
-      annotations.push(annotation);
     }
   }
 
