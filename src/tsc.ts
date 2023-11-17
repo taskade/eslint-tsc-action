d998600146258f237a93c0a4224b1e98d05f8c37
Duncan Leo <hello@duncanleo.me>
2023-11-17 14:26:16 +0800

    feat: use `ts-morph` to generate tsc annotations

--- src/tsc.ts
+++ src/tsc.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import { Project } from 'ts-morph';
 import * as typescript from 'typescript';
 
 import { Annotation } from './types';
@@ -11,102 +12,75 @@ export default async function tsc(
   const modulePath = path.join(process.cwd(), 'node_modules/typescript');
   const ts = (await import(modulePath)) as typeof typescript;
 
-  const configFileName = ts.findConfigFile(process.cwd(), ts.sys.fileExists);
+  const configFilePath = ts.findConfigFile(process.cwd(), ts.sys.fileExists);
 
-  if (configFileName == null) {
+  if (configFilePath == null) {
     throw new Error('config file not found!');
   }
-
-  const configFile = ts.readConfigFile(configFileName, ts.sys.readFile);
-
-  const compilerOptions = ts.parseJsonConfigFileContent(
-    configFile.config,
-    ts.sys,
-    process.cwd()
-  );
-
-  const compilerHost = ts.createCompilerHost(compilerOptions.options);
 
   const rootNames = [...filesToLint].map((file) => {
     return path.join(process.cwd(), file);
   });
   const annotations: Annotation[] = [];
 
-  console.log('[TSC] createProgram');
+  console.log('[TSC] create project');
   console.log('[NODE.JS] Memory usage', process.memoryUsage());
 
-  const program = ts.createProgram({
-    options: compilerOptions.options,
-    rootNames: compilerOptions.fileNames,
-    configFileParsingDiagnostics: compilerOptions.errors,
+  const project = new Project({
+    tsConfigFilePath: configFilePath,
+    skipAddingFilesFromTsConfig: true,
+    skipFileDependencyResolution: true,
+    libFolderPath: path.join(modulePath, './lib'),
   });
 
+  project.addSourceFilesAtPaths(rootNames);
+  project.resolveSourceFileDependencies();
+
+  console.log('[TSC] resolved file dependencies');
+  console.log('[NODE.JS] Memory usage', process.memoryUsage());
+
   for (const fileName of rootNames) {
-    const sourceFile = compilerHost.getSourceFile(
-      fileName,
-      compilerOptions.options.target ?? ts.ScriptTarget.Latest
-    );
+    const sourceFile = project.getSourceFile(fileName);
 
-    let allDiagnostics;
-
-    console.log(`[TSC] getPreEmitDiagnostics fileName=${fileName}`);
-    console.log('[NODE.JS] Memory usage', process.memoryUsage());
-
-    try {
-      allDiagnostics = ts.getPreEmitDiagnostics(program, sourceFile);
-    } catch (e) {
-      console.error(e);
+    if (sourceFile == null) {
+      console.warn(`[TSC] could not getSourceFile() for file=${fileName}`);
       continue;
     }
+
+    const allDiagnostics = sourceFile.getPreEmitDiagnostics();
+
+    console.log(`[TSC] getPreEmitDiagnostics file=${sourceFile.getFilePath()}`);
+    console.log('[NODE.JS] Memory usage', process.memoryUsage());
 
     console.log(`[TSC] Retrieved diagnostics length=${allDiagnostics.length}`);
     console.log('[NODE.JS] Memory usage', process.memoryUsage());
 
-    const formatHost: typescript.FormatDiagnosticsHost = {
-      getCanonicalFileName(fileName: string) {
-        return fileName;
-      },
-      getCurrentDirectory: ts.sys.getCurrentDirectory,
-      getNewLine() {
-        return ts.sys.newLine;
-      },
-    };
-
     for (const diagnostic of allDiagnostics) {
-      if (diagnostic.file == null || diagnostic.start == null) {
-        continue;
-      }
-
-      const filePath = path.relative(process.cwd(), diagnostic.file.fileName);
+      const filePath = path.relative(process.cwd(), sourceFile.getFilePath());
 
       if (!filesToLint.has(filePath)) {
         // Ignore files  that don't need annotations
         continue;
       }
 
-      const lineAndChar = diagnostic.file.getLineAndCharacterOfPosition(
-        diagnostic.start
-      );
+      const length = diagnostic.getLength() ?? 0;
+      const start_column = diagnostic.getStart() ?? 0;
+      const end_column = start_column + length;
 
-      const length = diagnostic.length ?? 0;
-
-      const start_column = lineAndChar.character + 1;
-      const end_column = lineAndChar.character + length;
-
-      const start_line = lineAndChar.line + 1;
+      const start_line = diagnostic.getLineNumber() ?? 0;
       const end_line = start_line;
 
       const annotation_level: Annotation['annotation_level'] = 'failure';
 
-      const message = ts
-        .formatDiagnostic(diagnostic, formatHost)
-        /**
-         * HACK: This removes most of the repeated file path in the formatted diagnostic message,
-         * which is unnecessary as the annotation is already displayed over its corresponding file.
-         */
-        .replace(filePath, '');
+      let message;
+      const messageText = diagnostic.getMessageText();
+      if (typeof messageText === 'string') {
+        message = messageText;
+      } else {
+        message = messageText.getMessageText();
+      }
 
-      const title = `TypeScript Compiler: Code ${diagnostic.code}`;
+      const title = `ts(${diagnostic.getCode()})`;
 
       const annotation: Annotation = {
         path: filePath,
